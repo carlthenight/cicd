f3381a8d0702370d6625e3cfe432756449ab7002
Max <carllee399@hotmail.com>
2021-12-21 16:09:40 +0800

    pipeline更换gitee仓库加速

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,7 +6,7 @@ pipeline {
             steps {
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
                     userRemoteConfigs: [[credentialsId: '5bb248b0-3562-4ac8-8d99-60df393e0248',
-                    url: 'https://github.com/carlthenight/cicd.git']]])
+                    url: 'https://gitee.com/carlthenight/cicd.git']]])
             }
         }
         stage('Build'){
