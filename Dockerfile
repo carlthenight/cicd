edd4c5d5672670f99213f44eeba45d22fbfcf4c8
Max <carllee399@hotmail.com>
2021-12-21 11:19:12 +0800

    fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM golang:alpine
 RUN mkdir /app
-COPY /gin-hello-world /app
+COPY . /app
 WORKDIR /app/gin-hello-world
 RUN go build -o main .
 CMD ["/app/gin-hello-world/main"]
--- pipeline.groovy
+++ pipeline.groovy
@@ -2,11 +2,27 @@ pipeline {
     agent any
 
     stages {
-        stage('Checkout') {
-            steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
-                    userRemoteConfigs: [[credentialsId: '5bb248b0-3562-4ac8-8d99-60df393e0248',
-                    url: 'https://github.com/carlthenight/cicd.git']]])
+        // stage('Checkout') {
+        //     steps {
+        //         checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
+        //             userRemoteConfigs: [[credentialsId: '5bb248b0-3562-4ac8-8d99-60df393e0248',
+        //             url: 'https://github.com/carlthenight/cicd.git']]])
+        //     }
+        // }
+        stage('Build'){
+            steps{
+                //构建Dockerfile定义的镜像
+                sh "docker build -t go-app-img ."
+                //使用yaml创建k3d集群
+                sh "k3d cluster create go -c ./cluster_init.yaml"
+                //查看nodes状况,是否创建集群成功
+                sh "kubectl get nodes"
+                //从docker导入镜像到k3d集群中
+                sh "k3d image import -c go-cluster go-gin-img"
+                //kubectl创建deployment
+                sh "kubectl create -f deployment.yaml"
+
+
             }
         }
     }
